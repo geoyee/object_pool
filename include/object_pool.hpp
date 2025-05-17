2b6dd66bfacdef92d9f232b4a06b8bf06b201522
geoyee <geoyee@yeah.net>
2025-05-17 17:58:35 +0800

    feat: Update for msvc

--- include/object_pool.hpp
+++ include/object_pool.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef __OBJECT_POOL_HPP__
 #define __OBJECT_POOL_HPP__
 
@@ -9,122 +11,194 @@
 #include <mutex>
 #include <stdexcept>
 
-static const size_t DEFAULT_CAPACITY = 1024;
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif // _MSC_VER
 
-template <typename T>
-class ObjectPool
+namespace opo
 {
-public:
-  using value_type = T;
-  using pointer = T*;
-  using const_pointer = const T*;
-  using reference = T&;
-  using const_reference = const T&;
-  using size_type = std::size_t;
-  using difference_type = std::ptrdiff_t;
-  using unique_arr_ptr_type = std::unique_ptr<T[]>;
-  using mutex_type = std::mutex;
-  using lock_type = std::lock_guard<mutex_type>;
-  using unique_lock_type = std::unique_lock<mutex_type>;
-  using shared_ptr_type = std::shared_ptr<T>;
-  using cleaner_type = std::function<void(T*)>;
-  using bitset_type = std::bitset<DEFAULT_CAPACITY>;
-
-public:
-  ObjectPool(
-      size_t capacity, cleaner_type cleaner = [](T* ptr) { (void)ptr; })
-    : capacity_(capacity), cleaner_(cleaner)
-  {
-    for (size_type i = 0; i < capacity_; ++i)
+    namespace _aux
     {
-      bitset_.set(i, true);
-    }
+        static constexpr size_t CRIRICAL_MASS = 64;
 
-    try
+        inline size_t _Find_Last_One(unsigned long block_)
+        {
+            if (block_ == 0)
+            {
+                return sizeof(unsigned long) * CHAR_BIT;
+            }
+#if defined(__GNUC__) || defined(__clang__)
+            return __builtin_ctzl(block_);
+#elif defined(_MSC_VER)
+            unsigned long index = 0;
+            _BitScanForward(&index, block_);
+            return index;
+#else  // Other compilers
+            size_t index = 0;
+            while ((block_ & 1) == 0)
+            {
+                block_ >>= 1;
+                ++count;
+            }
+            return index;
+#endif // __GNUC__ || __clang__
+        }
+
+        template<size_t N>
+        inline size_t _Find_First(const std::bitset<N> &bitset_)
+        {
+            if constexpr (N < CRIRICAL_MASS)
+            {
+                for (size_t i = 0; i < N; ++i)
+                {
+                    if (bitset_.test(i))
+                    {
+                        return i;
+                    }
+                }
+                return N;
+            }
+            else
+            {
+                constexpr size_t block_size = sizeof(unsigned long) * CHAR_BIT;
+                constexpr size_t num_blocks = (N + block_size - 1) / block_size;
+                unsigned long blocks[num_blocks] = { 0 };
+                memcpy(blocks, &bitset_, sizeof(blocks));
+                for (size_t i = 0; i < num_blocks; ++i)
+                {
+                    if (blocks[i] != 0)
+                    {
+                        size_t pos = i * block_size + _Find_Last_One(blocks[i]);
+                        return (pos < N) ? pos : N;
+                    }
+                }
+                return N;
+            }
+        }
+    } // namespace _aux
+
+    static constexpr size_t MAX_CAPACITY = 1024;
+
+    template<typename T>
+    class ObjectPool
     {
-      data_ = unique_arr_ptr_type(new T[capacity_]);
-    }
-    catch (std::bad_alloc& e)
+    public:
+        using value_type = T;
+        using pointer = T *;
+        using const_pointer = const T *;
+        using reference = T &;
+        using const_reference = const T &;
+        using size_type = std::size_t;
+        using difference_type = std::ptrdiff_t;
+        using unique_arr_ptr_type = std::unique_ptr<T[]>;
+        using mutex_type = std::mutex;
+        using lock_type = std::lock_guard<mutex_type>;
+        using unique_lock_type = std::unique_lock<mutex_type>;
+        using shared_ptr_type = std::shared_ptr<T>;
+        using cleaner_type = std::function<void(T *)>;
+        using bitset_type = std::bitset<MAX_CAPACITY>;
+
+    public:
+        ObjectPool(
+            size_t capacity, cleaner_type cleaner = [](T *ptr) { (void)ptr; })
+            : capacity_(capacity)
+            , cleaner_(cleaner)
+        {
+            for (size_type i = 0; i < capacity_; ++i)
+            {
+                bitset_.set(i, true);
+            }
+            try
+            {
+                data_ = unique_arr_ptr_type(new T[capacity_]);
+            }
+            catch (std::bad_alloc &e)
+            {
+                throw std::runtime_error("ObjectPool::ObjectPool: bad alloc, due to " + std::string(e.what()));
+                capacity_ = 0;
+                bitset_.reset();
+            }
+        }
+
+        ObjectPool(const ObjectPool &) = delete;
+        ObjectPool(ObjectPool &&) = delete;
+        ObjectPool &operator=(const ObjectPool &) = delete;
+        ObjectPool &operator=(ObjectPool &&) = delete;
+
+        ~ObjectPool()
+        {
+            for (size_type i = 0; i < capacity_; ++i)
+            {
+                if (bitset_[i])
+                {
+                    cleaner_(&data_[i]);
+                }
+            }
+        }
+
+        shared_ptr_type get_shared_pointer()
+        {
+            return std::shared_ptr<value_type>(alloc(),
+                                               [this](value_type *ptr) {
+                if (ptr)
+                {
+                    cleaner_(ptr);
+                    free(ptr);
+                }
+            });
+        }
+
+        size_type capacity() const noexcept
+        {
+            return capacity_;
+        }
+
+        size_type size() noexcept
+        {
+            std::lock_guard<mutex_type> lock(mutex_);
+            return capacity_ - bitset_.count();
+        }
+
+        pointer alloc() noexcept
+        {
+            std::lock_guard<mutex_type> lock(mutex_);
+#if defined(__GNUC__) || defined(__clang__)
+            size_type index = bitset_._Find_first();
+#else
+            size_type index = _aux::_Find_First(bitset_);
+#endif // __GNUC__ || __clang__
+            if (index == bitset_.size())
+            {
+                return nullptr;
+            }
+            bitset_.set(index, false);
+            return &data_[index];
+        }
+
+        void free(pointer ptr) noexcept
+        {
+            std::lock_guard<mutex_type> lock(mutex_);
+            bitset_.set(ptr - data_.get(), true);
+        }
+
+    protected:
+        size_type capacity_{ 0 };
+        unique_arr_ptr_type data_{ nullptr };
+        mutex_type mutex_;
+        cleaner_type cleaner_;
+        bitset_type bitset_;
+    };
+
+    template<typename T>
+    inline std::shared_ptr<T> get_shared_pointer_from(ObjectPool<T> &pool)
     {
-      throw e;
-      capacity_ = 0;
-      bitset_.reset();
+        auto ptr = pool.get_shared_pointer();
+        if (!ptr)
+        {
+            ptr = std::make_shared<T>();
+        }
+        return ptr;
     }
-  }
+} // namespace opo
 
-  ObjectPool(const ObjectPool&) = delete;
-  ObjectPool(ObjectPool&&) = delete;
-  ObjectPool& operator=(const ObjectPool&) = delete;
-  ObjectPool& operator=(ObjectPool&&) = delete;
-
-  ~ObjectPool()
-  {
-    for (size_type i = 0; i < capacity_; ++i)
-    {
-      if (bitset_[i])
-      {
-        cleaner_(&data_[i]);
-      }
-    }
-  }
-
-  shared_ptr_type get_shared_pointer()
-  {
-    return std::shared_ptr<value_type>(alloc(), [this](value_type* ptr) {
-      if (ptr)
-      {
-        cleaner_(ptr);
-        free(ptr);
-      }
-    });
-  }
-
-  size_type capacity() const noexcept
-  {
-    return capacity_;
-  }
-
-  size_type size() noexcept
-  {
-    std::lock_guard<mutex_type> lock(mutex_);
-    return capacity_ - bitset_.count();
-  }
-
-  pointer alloc() noexcept
-  {
-    std::lock_guard<mutex_type> lock(mutex_);
-    size_type index = bitset_._Find_first();
-    if (index == bitset_.size())
-    {
-      return nullptr;
-    }
-    bitset_.set(index, false);
-    return &data_[index];
-  }
-
-  void free(pointer ptr) noexcept
-  {
-    std::lock_guard<mutex_type> lock(mutex_);
-    bitset_.set(ptr - data_.get(), true);
-  }
-
-protected:
-  size_type capacity_{ 0 };
-  unique_arr_ptr_type data_{ nullptr };
-  mutex_type mutex_;
-  cleaner_type cleaner_;
-  bitset_type bitset_;
-};
-
-template <typename T>
-std::shared_ptr<T> get_shared_pointer_from(ObjectPool<T>& pool)
-{
-  auto ptr = pool.get_shared_pointer();
-  if (!ptr)
-  {
-    ptr = std::make_shared<T>();
-  }
-  return ptr;
-}
-
-#endif
+#endif // __OBJECT_POOL_HPP__
--- src/demo.cpp
+++ src/demo.cpp
@@ -1,74 +1,86 @@
 #include "object_pool.hpp"
+
+#include <chrono>
+#include <cstdio>
+#include <iostream>
+#include <memory>
 #include <thread>
 #include <vector>
-#include <chrono>
-#include <memory>
-#include <cstdio>
+
+using namespace opo;
 
 struct Object
 {
-  int a;
-  int b;
-  int c;
-  int d;
-  int e;
-  int f;
-  int g;
-  double h;
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    double h;
 };
 
-int main(int argc, const char* const* argv)
+int main(int argc, const char *const *argv)
 {
-  if (argc < 2)
-  {
-    printf("Usage: %s <0: object_pool, 1: std::make_shared()>\n", argv[0]);
-    return 1;
-  }
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <0: object_pool, 1: std::make_shared>\n" << std::endl;
+        return 1;
+    }
 
-  auto choice = atoi(argv[1]);
+    auto choice = atoi(argv[1]);
 
-  if (choice == 0)
-  {
-    printf("Using object_pool\n");
-  }
-  else
-  {
-    printf("Using std::make_shared()\n");
-  }
+    if (choice == 0)
+    {
+        std::cout << "Using object_pool" << std::endl;
+    }
+    else
+    {
+        std::cout << "Using std::make_shared" << std::endl;
+    }
 
-  ObjectPool<Object> pool(256);
-  std::vector<std::thread> threads;
-  for (int i = 0; i < 8; ++i)
-  {
-    threads.emplace_back([&pool, i, choice]() {
-      for (int i = 0; i < 65536; ++i)
-      {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        std::shared_ptr<Object> obj;
-        if (choice == 0)
-        {
-          obj = get_shared_pointer_from(pool);
-        }
-        else
-        {
-          obj = std::make_shared<Object>();
-        }
-        obj->a = i;
-        obj->b = i;
-        obj->c = i;
-        obj->d = i;
-        obj->e = i;
-        obj->f = i;
-        obj->g = i;
-        obj->h = i;
-      }
-    });
-  }
+    ObjectPool<Object> pool(256);
 
-  for (auto& t : threads)
-  {
-    t.join();
-  }
+    auto beforeTime = std::chrono::steady_clock::now();
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 8; ++i)
+    {
+        threads.emplace_back([&pool, choice]() {
+            for (int i = 0; i < 512; ++i)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                std::shared_ptr<Object> obj;
+                if (choice == 0)
+                {
+                    // std::cout << "Get obj from pool" << std::endl;
+                    obj = get_shared_pointer_from(pool);
+                }
+                else
+                {
+                    // std::cout << "Create obj by make_shared" << std::endl;
+                    obj = std::make_shared<Object>();
+                }
+                obj->a = i;
+                obj->b = i;
+                obj->c = i;
+                obj->d = i;
+                obj->e = i;
+                obj->f = i;
+                obj->g = i;
+                obj->h = i;
+            }
+        });
+    }
 
-  return 0;
+    for (auto &t : threads)
+    {
+        t.join();
+    }
+
+    auto afterTime = std::chrono::steady_clock::now();
+    double durationMillsecond = std::chrono::duration<double, std::milli>(afterTime - beforeTime).count();
+    std::cout << "Use " << durationMillsecond << " ms" << std::endl;
+
+    return 0;
 }
